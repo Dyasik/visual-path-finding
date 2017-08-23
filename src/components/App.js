9a041955406c3b7bfc304ec5c514530e10af510b
Denis Seletkov <rysds2@gmail.com>
2017-08-23 23:19:30 +0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>Hi)0))0))0)</div>;
+    }
+}
